3ed0338e6f3a6d6f4b9f798742bd895e8ebdff03
Revoulse <100707647+Gradyyy@users.noreply.github.com>
2024-03-21 21:43:03 +0700

    Update Cors 4

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,6 +18,7 @@ class CorsMiddleware
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', 'https://kofebin.vercel.app')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }
