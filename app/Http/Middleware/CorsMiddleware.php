29bed91872553ce3582a3984942d4c7e7bcf1219
Revoulse <100707647+Gradyyy@users.noreply.github.com>
2024-03-21 22:08:26 +0700

    Update Cors 8
    Update Cors 8

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,16 +17,45 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
 
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers' => 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-            'Access-Control-Max-Age' => '86400',
-            'Access-Control-Allow-Credentials' => 'true'
-        ];
+        // $headers = [
+        //     'Access-Control-Allow-Origin' => '*',
+        //     'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+        //     'Access-Control-Allow-Headers' => 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+        //     'Access-Control-Max-Age' => '86400',
+        //     'Access-Control-Allow-Credentials' => 'true'
+        // ];
+
+        // if ($request->isMethod('OPTIONS')) {
+        //     return response()->json('OK', 200, $headers);
+        // }
+
+        // $response = $next($request);
+
+        // foreach ($headers as $key => $value) {
+        //     $response->header($key, $value);
+        // }
+
+        // return $response;
+
+        // Define the allowed origin for your frontend
+        $allowedOrigins = ['https://kofebin.vercel.app'];
+
+        // Check if the request origin is allowed
+        $origin = $request->headers->get('Origin');
+        if (in_array($origin, $allowedOrigins)) {
+            $headers = [
+                'Access-Control-Allow-Origin' => $origin,
+                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials' => 'true', // Enable credentials
+                'Access-Control-Max-Age' => '86400', // 24 hours
+            ];
+        } else {
+            $headers = [];
+        }
 
         if ($request->isMethod('OPTIONS')) {
-            return response()->json('OK', 200, $headers);
+            return response('OK', 200, $headers);
         }
 
         $response = $next($request);
