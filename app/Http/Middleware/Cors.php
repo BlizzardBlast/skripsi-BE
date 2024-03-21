7e01430a69d7d1525b95509dd91c8b011f631973
BlizzardBlast <fdsurya7@gmail.com>
2024-03-21 20:39:02 +0700

    fix: BE

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', '[http://localhost:5173/, https://kofebin.vercel.app/]');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
