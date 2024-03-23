5c11100fdd35976346b8425068df3959af44ed6b
BlizzardBlast <fdsurya7@gmail.com>
2024-03-23 11:07:47 +0700

    hmm

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -46,7 +46,6 @@ class CorsMiddleware
             $headers = [
                 'Access-Control-Allow-Origin' => $origin,
                 'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
                 'Access-Control-Allow-Credentials' => 'true', // Enable credentials
                 'Access-Control-Max-Age' => '86400', // 24 hours
             ];
